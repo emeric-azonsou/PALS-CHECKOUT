36a760ec42fd9fa8eba729b47823cf6cac8de43d
EmericAz <azonsou158@gmail.com>
2022-07-30 16:31:21 +0100

    responssive

--- src/app/component/unsuccesfull-transaction/unsuccesfull-transaction.component.ts
+++ src/app/component/unsuccesfull-transaction/unsuccesfull-transaction.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unsuccesfull-transaction',
   templateUrl: './unsuccesfull-transaction.component.html',
-  styleUrls: ['./unsuccesfull-transaction.component.scss']
+  styleUrls: ['./unsuccesfull-transaction.component.scss'],
 })
 export class UnsuccesfullTransactionComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onRetryPayement() {
+    this.router.navigate(['allow']);
   }
-
 }
